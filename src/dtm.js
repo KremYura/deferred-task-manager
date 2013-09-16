7dcdc672f165181d90566177bc9257eb4dcd8e49
Yuri Kremenchutskiy <kremyura@gmail.com>
2013-09-17 00:44:11 +0300

    DTM.Task moved to DTM

--- src/dtm.js
+++ src/dtm.js
@@ -6,6 +6,57 @@ var DTM = function(params) {
 	
 	this.onStart = params.onStart || this.onStart;
 	this.onFinish = params.onFinish || this.onFinish;
+
+	this.Task = function(name, params, dtm) {
+		var self = this;
+		
+		params = params || {};
+
+		this.name = name;
+		this.single = params.single || false;
+		this.collection = [].concat(params.collection || []);
+		this.delay = params.delay || 0;
+		this.partialItemsCount = params.partialItemsCount || 1;
+		this.deferredFunction = params.deferredFunction || function(partialItems) { this.endTask(); };
+		this.timeout = undefined;
+
+		if (this.deferredFunction.toString().indexOf('.endTask()') == -1) {
+			alert('Task: "'+name+'" initialized without endTask method call. Please add this.endTask(); in deferredFunction logic');
+		}
+
+		this.getPartialItems = function() {
+			return this.collection.splice(0, this.partialItemsCount || 1);
+		};
+
+		this.runDeferredFunction = function(partialItems) {
+			var status = this.deferredFunction.call(this, partialItems);
+
+			return status;
+		};
+
+		this.extendCollection = function(collection) {
+			this.collection.push.apply(this.collection, collection);
+			//var _spliceHelper = [0,0];
+			//_spliceHelper.push.apply(_spliceHelper, collection);
+			//this.collection.splice.apply(this.collection, _spliceHelper);
+		};
+
+		this.startTask = function() {
+			this.timeout = setTimeout(function() {
+				var data = self.getPartialItems();
+				self.runDeferredFunction(data);
+			}, this.delay);
+		};
+
+		this.endTask = function() {
+			this.timeout = null;
+			if (this.collection.length > 0) {
+				this.startTask();
+			} else {
+				dtm.unregister(this.name);
+			}
+		};
+	};
 };
 
 DTM.prototype = {
@@ -28,7 +79,7 @@ DTM.prototype = {
 		if (this.tasks[name]) {
 			this.tasks[name].extendCollection(params.collection);
 		} else {
-			this.tasks[name] = new DTM.Task(name, params, this);
+			this.tasks[name] = new this.Task(name, params, this);
 			this.taskCount++;
 			this.tasks[name].startTask();
 		}
@@ -52,55 +103,4 @@ DTM.prototype = {
 			this.unregister(name);
 		}
 	}
-};
-
-DTM.Task = function(name, params, dtm) {
-	var self = this;
-	
-	params = params || {};
-
-	this.name = name;
-	this.single = params.single || false;
-	this.collection = [].concat(params.collection || []);
-	this.delay = params.delay || 0;
-	this.partialItemsCount = params.partialItemsCount || 1;
-	this.deferredFunction = params.deferredFunction || function(partialItems) { this.endTask(); };
-	this.timeout = undefined;
-
-	if (this.deferredFunction.toString().indexOf('.endTask()') == -1) {
-		alert('Task: "'+name+'" initialized without endTask method call. Please add this.endTask(); in deferredFunction logic');
-	}
-
-	this.getPartialItems = function() {
-		return this.collection.splice(0, this.partialItemsCount || 1);
-	};
-
-	this.runDeferredFunction = function(partialItems) {
-		var status = this.deferredFunction.call(this, partialItems);
-
-		return status;
-	};
-
-	this.extendCollection = function(collection) {
-		this.collection.push.apply(this.collection, collection);
-		//var _spliceHelper = [0,0];
-		//_spliceHelper.push.apply(_spliceHelper, collection);
-		//this.collection.splice.apply(this.collection, _spliceHelper);
-	};
-
-	this.startTask = function() {
-		this.timeout = setTimeout(function() {
-			var data = self.getPartialItems();
-			self.runDeferredFunction(data);
-		}, this.delay);
-	};
-
-	this.endTask = function() {
-		this.timeout = null;
-		if (this.collection.length > 0) {
-			this.startTask();
-		} else {
-			dtm.unregister(this.name);
-		}
-	};
 };
