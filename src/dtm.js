2fe361beeb6b2c8614d754241f7477f20b2e7a81
KremYura <kremyura@gmail.com>
2015-12-01 00:36:30 +0200

    moved to module

--- src/dtm.js
+++ src/dtm.js
@@ -1,111 +1,117 @@
-var DTM = function(params) {
-	params = params || {};
+( function( exports ) {
+	'use strict';
 	
-	this.tasks = { };
-	this.taskCount = 0;
-	
-	this.onStart = params.onStart || this.onStart;
-	this.onFinish = params.onFinish || this.onFinish;
-
-	this.Task = function(name, params, dtm) {
-		var self = this;
-		
+	function DTM(params) {
 		params = params || {};
+		
+		this.tasks = { };
+		this.taskCount = 0;
+		
+		this.onStart = params.onStart || this.onStart;
+		this.onFinish = params.onFinish || this.onFinish;
 
-		this.name = name;
-		this.single = params.single || false;
-		this.collection = [].concat(params.collection || []);
-		this.delay = params.delay || 0;
-		this.partialItemsCount = params.partialItemsCount || 1;
-		this.deferredFunction = getTaskFunction(params.deferredFunction);
-		this.timeout = undefined;
+		this.Task = function(name, params, dtm) {
+			var self = this;
+			
+			params = params || {};
 
-		function getTaskFunction(func) {
-			return function() {
-				func && func.apply(this, arguments);
-				this.endTask();
+			this.name = name;
+			this.single = params.single || false;
+			this.collection = [].concat(params.collection || []);
+			this.delay = params.delay || 0;
+			this.partialItemsCount = params.partialItemsCount || 1;
+			this.deferredFunction = getTaskFunction(params.deferredFunction);
+			this.timeout = undefined;
+
+			function getTaskFunction(func) {
+				return function() {
+					func && func.apply(this, arguments);
+					this.endTask();
+				}
 			}
-		}
 
-		this.getPartialItems = function() {
-			return this.collection.splice(0, this.partialItemsCount || 1);
-		};
+			this.getPartialItems = function() {
+				return this.collection.splice(0, this.partialItemsCount || 1);
+			};
 
-		this.runDeferredFunction = function(partialItems) {
-			var status = this.deferredFunction.call(this, partialItems);
+			this.runDeferredFunction = function(partialItems) {
+				var status = this.deferredFunction.call(this, partialItems);
 
-			return status;
-		};
+				return status;
+			};
 
-		this.extendCollection = function(collection) {
-			this.collection.push.apply(this.collection, collection);
-			//var _spliceHelper = [0,0];
-			//_spliceHelper.push.apply(_spliceHelper, collection);
-			//this.collection.splice.apply(this.collection, _spliceHelper);
-		};
+			this.extendCollection = function(collection) {
+				this.collection.push.apply(this.collection, collection);
+				//var _spliceHelper = [0,0];
+				//_spliceHelper.push.apply(_spliceHelper, collection);
+				//this.collection.splice.apply(this.collection, _spliceHelper);
+			};
 
-		this.startTask = function() {
-			this.timeout = setTimeout(function() {
-				var data = self.getPartialItems();
-				self.runDeferredFunction(data);
-			}, this.delay);
-		};
+			this.startTask = function() {
+				this.timeout = setTimeout(function() {
+					var data = self.getPartialItems();
+					self.runDeferredFunction(data);
+				}, this.delay);
+			};
 
-		this.endTask = function() {
-			this.timeout = null;
-			if (this.collection.length > 0) {
-				this.startTask();
-			} else {
-				dtm.unregister(this.name);
-			}
+			this.endTask = function() {
+				this.timeout = null;
+				if (this.collection.length > 0) {
+					this.startTask();
+				} else {
+					dtm.unregister(this.name);
+				}
+			};
 		};
 	};
-};
 
-DTM.prototype = {
-	onStart: function() {
-		//console.log('onStart');
-	},
-	onFinish: function() {
-		//console.log('onFinish');
-	},
-	add: function(name, params) {
-		if (this.taskCount === 0) {
-			this.onStart();
-		}
-
-		if (params.single) {
-			this.unregister(name);
-			params.collection = ["single"];
-		}
-
-		if (this.tasks[name]) {
-			this.tasks[name].extendCollection(params.collection);
-		} else {
-			this.tasks[name] = new this.Task(name, params, this);
-			this.taskCount++;
-			this.tasks[name].startTask();
-		}
-	},
-	unregister: function(name) {
-		if (this.tasks[name]) {
-			if (this.tasks[name].timeout) {
-				clearTimeout(this.tasks[name].timeout);
-				this.tasks[name].timeout = null;
+	DTM.prototype = {
+		onStart: function() {
+			//console.log('onStart');
+		},
+		onFinish: function() {
+			//console.log('onFinish');
+		},
+		add: function(name, params) {
+			if (this.taskCount === 0) {
+				this.onStart();
 			}
-			this.taskCount--;
-			delete this.tasks[name];
-		}
 
-		if (this.taskCount === 0) {
-			this.onFinish();
-		}
-	},
-	unregisterAll: function() {
-		for (var name in this.tasks) {
-			if ( this.tasks.hasOwnProperty(name) ) {
+			if (params.single) {
 				this.unregister(name);
+				params.collection = ["single"];
+			}
+
+			if (this.tasks[name]) {
+				this.tasks[name].extendCollection(params.collection);
+			} else {
+				this.tasks[name] = new this.Task(name, params, this);
+				this.taskCount++;
+				this.tasks[name].startTask();
+			}
+		},
+		unregister: function(name) {
+			if (this.tasks[name]) {
+				if (this.tasks[name].timeout) {
+					clearTimeout(this.tasks[name].timeout);
+					this.tasks[name].timeout = null;
+				}
+				this.taskCount--;
+				delete this.tasks[name];
+			}
+
+			if (this.taskCount === 0) {
+				this.onFinish();
+			}
+		},
+		unregisterAll: function() {
+			for (var name in this.tasks) {
+				if ( this.tasks.hasOwnProperty(name) ) {
+					this.unregister(name);
+				}
 			}
 		}
-	}
-};
+	};
+
+	exports.DTM = DTM;
+}( this ) );
